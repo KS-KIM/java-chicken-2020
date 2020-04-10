21e5be94f480371b90dcddd5b84e58d40f9a20c6
KS-KIM <nullable@kakao.com>
2020-04-10 14:41:26 +0900

    refactor: 정적 유틸리티 클래스의 인스턴스 생성 방지용 생성자 추가
    * indent를 tab 문자로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,10 +3,13 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
-    }
+	private InputView() {
+	}
+
+	public static int inputTableNumber() {
+		System.out.println("## 주문할 테이블을 선택하세요.");
+		return scanner.nextInt();
+	}
 }
