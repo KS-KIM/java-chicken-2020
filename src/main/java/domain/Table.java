ac81ac047f51e56176c7034ac54182fc1602a63e
KS-KIM <nullable@kakao.com>
2020-04-10 14:38:24 +0900

    feat: 메뉴, 테이블 비교 연산자 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,14 +1,31 @@
 package domain;
 
+import java.util.Objects;
+
 public class Table {
-    private final int number;
+	private final int number;
 
-    public Table(final int number) {
-        this.number = number;
-    }
+	public Table(final int number) {
+		this.number = number;
+	}
 
-    @Override
-    public String toString() {
-        return Integer.toString(number);
-    }
+	@Override
+	public String toString() {
+		return Integer.toString(number);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Table table = (Table)o;
+		return number == table.number;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(number);
+	}
 }
